9a56f1c3ae32c916b0cb662479a9953764212880
samuelebumbello <samuelebumbello17@gmail.com>
2023-04-03 17:25:50 +0200

    [add] array object info squad

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
-// ELEMENTS \\
-const
+// Creo un'array di object con le info dei colleghi \\
+const team = [{
+    firstname: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image : 'wayne-barnett-founder-ceo.jpg',
+},
+{
+    firstname: 'Angela Caroll',
+    role: 'Chief Editor',
+    image : 'angela-caroll-chief-editor.jpg',
+},
+{
+    firstname: 'Walter Gordon',
+    role: 'Front-End Developer',
+    image : 'walter-gordon-office-manager.jpg',
+},
+{
+    firstname: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image : 'angela-lopez-social-media-manager.jpg',
+},
+{
+    firstname: 'Scott Estrada',
+    role: 'Developer',
+    image : 'scott-estrada-developer.jpg',
+},
+{
+    firstname: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image : 'barbara-ramos-graphic-designer.jpg',
+},
+];
+
+
